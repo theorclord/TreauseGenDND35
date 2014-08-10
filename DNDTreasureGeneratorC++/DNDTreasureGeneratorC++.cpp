b32d88b90fdde536061aeed438340c5387f3a5a0
Mikkel <mikkeltheorclord@hotmail.com>
2014-08-10 16:35:27 +0200

    initial commit

---
+++ DNDTreasureGeneratorC++/DNDTreasureGeneratorC++.cpp
@@ -0,0 +1,14 @@
+// DNDTreasureGeneratorC++.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	printf("2");
+	return 0;
+}
+
